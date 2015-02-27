5704102fb4477b68bed9c2b7601ba059004ea6ad
Martin Jorn Rogalla <martin@martinrogalla.com>
2015-02-27 14:32:41 +0100

    Started using objects and make it less static.
    Signed-off-by: Martin Jorn Rogalla <martin@martinrogalla.com>

--- js/ball.js
+++ js/ball.js
@@ -1,38 +1,67 @@
 var svg;
 
+var w = 800, h = 600;
+var balls;
+var boundaries;
+var collided;
 var sub;
 var observer;
 var hot;
-var w = 800, h = 600;
-var t0 = Date.now();
-var balls;
-var grounds;
-var collided;
+
+var ball_source;
+
+/*
+ * Ball
+ */
+function Ball(x,y,r,a){
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.acc = a;
+}
+
+/*
+ * Ground
+ */
+function Ground(x,y,h,w){
+  this.x = x;
+  this.y = y;
+  this.h = h;
+  this.w = w;
+}
 
 var init = function(){
   console.log("Initializing");
 
   balls = [
-    { x: w/2, y: h/8, r: 20, acc: 1.03}
+    new Ball(w/2,h/8,20,1.03)
   ];
-  grounds = [
-    { x: 0, y: h-20, "width": w, "height": 20}
+  boundaries = [
+    new Ground(0,h-20,20,w)
   ];
 
   var collided = false;
 
   svg = d3.select("#planetarium").insert("svg") .attr("width", w).attr("height", h);
-  svg.append("circle").attr("r", 20).attr("cx", w/2).attr("cy", h/8).attr("class", "ball")
-  svg.append("rect").attr("x", 0).attr("y", h-20).attr("height", 20).attr("width", w).attr("class", "ground")
+  
+  for(var i in boundaries) {
+    var boundary = boundaries[i];
+    svg.append("rect").attr("x", boundary.x).attr("y", boundary.y).attr("height", boundary.h).attr("width", boundary.w).attr("class", "boundary")
+    console.log("Boundary added.");
+  }
+  for(var i in balls) {
+    var ball = balls[i];
+    svg.append("circle").attr("r", ball.r).attr("cx", ball.x).attr("cy", ball.y).attr("class", "ball")
+    console.log("Ball added.");
+  }
 }
 
 var collision = function(){
   for(var i in balls) {
     var ball = balls[i];
-    for(var j in grounds) {
-      var ground = grounds[j];
-      if((ball.y*ball.acc)>h-40){
-        console.log(h-ball.y)
+    for(var j in boundaries) {
+      var boundary = boundaries[j];
+      if((ball.y*ball.acc)>boundary.y-ball.r){
         return true;
       }
     }
@@ -50,7 +79,6 @@ var startReactive = function(){
   hot = source.publish();
   observer = Rx.Observer.create(
     function (x) {
-      var delta = (Date.now() - t0);
       if(!collision()){
         svg.selectAll(".ball").data(balls).attr("cy", function(d) { return d.y=d.y*d.acc; });
       }
