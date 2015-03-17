4482d9b13b30c0475c3b3858b862889e53e1e4e9
Martin Jorn Rogalla <martin@martinrogalla.com>
2015-03-17 10:17:09 +0100

    Corrected usage of requirejs.
    Signed-off-by: Martin Jorn Rogalla <martin@martinrogalla.com>

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-requirejs(["../ext/jquery/dist/jquery.min",
-        "../ext/bootstrap/dist/js/bootstrap.min",
-        "../ext/rxjs/dist/rx.all.min",
-        "../ext/d3/d3.min",
-        "./physics",
-        "./shapes",
-        "./time",
-        "./utils",
-        "./view",
-        "./vs"],
-    function ($, bootstrap, rx, d3, vs) {
-        init();
-    });
+require.config({
+    paths: {
+        d3: '../ext/d3/d3.min',
+        bootstrap: '../ext/bootstrap/dist/js/bootstrap.min',
+        jquery: '../ext/jquery/dist/jquery.min',
+        rx: '../ext/rxjs/dist/rx.all.min',
+        physics: 'physics',
+        shapes: 'shapes',
+        time: 'time',
+        utils: 'utils',
+        view: 'view',
+        app: 'app'
+    },
+    shim: {
+        app: {
+            deps: ['utils', 'view'],
+            exports: 'app'
+        },
+        bootstrap: {
+            deps: ['jquery']
+        },
+        jquery: {
+            exports: 'jQuery'
+        }
+    }
+});
+
+require( ["app","bootstrap","d3","jquery","physics","rx","shapes","time","utils","view"], function (app) {
+    init();
+});
