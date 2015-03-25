695b9f12460ba893cbeb902c9f26cfc24bf93bdb
Joop Aué <joopaue@gmail.com>
2015-03-25 11:29:17 +0100

    Circle instance methods are moved from constructor
    Makes things clearer
    
    Signed-off-by: Joop Aué <joopaue@gmail.com>

--- js/shapes/circle.js
+++ js/shapes/circle.js
@@ -16,29 +16,29 @@ function Circle(position, radius, velocity, restitution) {
   this.mass = 1; //radius * radius * Math.PI;
   this.velocity = velocity;
   this.restitution = restitution;
+}
 
-  /**
+/**
    * Update the velocity of the circle by applying gravity.
    * @param gravity - the gravity vector.
    */
-  this.applyGravity = function (gravity) {
-    this.velocity = Vector.add(this.velocity, gravity);
-  };
+Circle.prototype.applyGravity = function (gravity) {
+  this.velocity = Vector.add(this.velocity, gravity);
+};
 
-  /**
+/**
    * Update the position of the circle.
    * @param dt - the change in time.
    */
-  this.updatePosition = function (dt) {
-    var distanceVector = Vector.multiply(this.velocity, dt);
-    this.position = Vector.add(this.position, distanceVector);
-  };
+Circle.prototype.updatePosition = function (dt) {
+  var distanceVector = Vector.multiply(this.velocity, dt);
+  this.position = Vector.add(this.position, distanceVector);
+};
 
-  /**
+/**
    * Update the radius of the circle.
    * @param delta_radius - the change in radius.
    */
-  this.updateRadius = function (delta_radius) {
-    this.radius += delta_radius;
-  };
+Circle.prototype.updateRadius = function (delta_radius) {
+  this.radius += delta_radius;
 };
