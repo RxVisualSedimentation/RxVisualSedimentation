3ebcd4632f3c5e755e45dfd2ce5fda04bdcb30cc
Joop Aué <joopaue@gmail.com>
2015-04-03 15:14:45 +0200

    Incoming tweets are handled reactively
    Signed-off-by: Joop Aué <joopaue@gmail.com>

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -6,28 +6,48 @@ var initWebsocket = function () {
   if (!window.WebSocket) {
     console.log("Your browser does not support websockets");
     return;
-  } 
+  }
 
   connection.onopen = function () {
     console.log("Connection succesfully opened");
   }
 
-  connection.onerror = function (error) { 
-    console.log("Error occurred: ");
-    console.log(error.toString());
-  }
-
-  connection.onmessage = function (message) {
-    try {
-      var tweet = JSON.parse(message.data);
-    } catch (e) {
-      console.log('This doesn\'t look like a valid JSON: ', message.data);
-      return;
+  //The function given as parameter to create will only get executed when subscribe is called on tweetObservable.
+  var tweetObservable = Rx.Observable.create(function (observer) {
+    connection.onmessage = function (message) {
+      observer.onNext(message);
     }
-    console.log(tweet.created_at + " - " + tweet.user.name);
-  }  
+
+    connection.onerror = function (error) {
+      observer.onError(error);
+    }
+
+    connection.onclose = function () {
+      observer.onCompleted();
+    }
+  });
+
+  tweetObservable.subscribe(
+    function (message) {
+      try {
+        var tweet = JSON.parse(message.data);
+      } catch (e) {
+        console.log('This doesn\'t look like a valid JSON: ', message.data);
+        return;
+      }
+      console.log(tweet.created_at + " - " + tweet.user.name);
+    },
+    function (error) {
+      console.log("Error occurred: ");
+      console.log(error.toString());
+    },
+    function () {
+      console.log('Completed');
+    });
 }
-  
+
+
+
 //  input.keydown(function(e) {
 //    if (e.keyCode === 13) {
 //      var msg = $(this).val();
